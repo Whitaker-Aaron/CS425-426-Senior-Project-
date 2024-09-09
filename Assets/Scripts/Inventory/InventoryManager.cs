e182ab333daa8466bd7c47a880058cdce2592d62
MrBoatCraft <stephenwest@unr.edu>
2024-09-08 17:47:17 -0700

    Updated Inventory
    Adding cell logic

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public GameObject currentInHand;
+    public Transform cellParent;
     public Vector3 offset;
     bool open = false;
     public GameObject Menu;
@@ -16,7 +17,7 @@ public class InventoryManager : MonoBehaviour
     private void Start()
     {
         //Loading all the items
-        //SetList();
+        SetList();
     }
 
     private void Update()
@@ -26,7 +27,27 @@ public class InventoryManager : MonoBehaviour
 
     public void SetList()
     {
-        //This is where we can manage the sorting of weapons based off of class and everything else
+        int index = 0;
+
+        foreach (Transform cell in cellParent)
+        {
+            if (cell.childCount > 0)
+            {
+                foreach (Transform child in cell)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+
+            if (Inventory.instance.inventoryItems[index] != null)
+            {
+                if (Inventory.instance.inventoryItems[index].GetComponent<Item_Script>().heldProperties != null)
+                {
+                    //cell.GetComponent<CellScript>().CreateItemInCell(Inventory.instance.inventoryItems[index].GetComponent<Item_Script>().heldProperties);
+                }
+
+            }
+        }
     }
     void OpenClose()
     {
